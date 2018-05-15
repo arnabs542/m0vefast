3418354d3cdd5e180a224738ae4e6b039ae0d6ab
siliang625 <siliang625@users.noreply.github.com>
2018-05-15 09:14:08 -0700

    20180515_morning

--- linkedlist/hasCycle.java
+++ linkedlist/hasCycle.java
@@ -1,12 +1,34 @@
 public boolean hasCycle(ListNode head){
-	ListNode fast = head;
-	ListNode slow = head;
-	while(fast.next!= null){
-	//while(fast != null && fast.next != null){
-		slow = slow.next;
-		fast = fast.next.next;
-		if(slow == fast)
+		if (head == null || head.next == null)
+			return false;
+		ListNode fast = head.next;
+		ListNode slow = head;
+			while (fast != slow) {
+					if(fast==null || fast.next==null)
+							return false;
+					fast = fast.next.next;
+					slow = slow.next;
+			}
 			return true;
 	}
-	return false;
-}
+
+//Return the node where the cycle starts. Return null if there is no cycle.
+if (head == null || head.next==null) {
+            return null;
+        }
+
+        ListNode fast, slow;
+        fast = head.next;
+        slow = head;
+        while (fast != slow) {
+            if(fast==null || fast.next==null)
+                return null;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        while (head != slow.next) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
--- tree_basic/recoverBST.java
+++ tree_basic/recoverBST.java
@@ -1,6 +1,13 @@
 //2 nodes not in correct position, recover the Tree
+//O(n), O(LogN)
+http://blog.sina.com.cn/s/blog_eb52001d0102v1z3.html
+Wrong order: 1 3 8 6 7 4 10 13 14
+FIND:            8 6
+Then we find:        7 4
+            swap 8 and 4
     private TreeNode firstElement = null;
     private TreeNode secondElement = null;
+    //in order traversal, should be ascending
     private TreeNode lastElement = new TreeNode(Integer.MIN_VALUE);
 
     private void traverse(TreeNode root) {
@@ -8,9 +15,11 @@
             return;
         }
         traverse(root.left);
+         //8: make sure the first occurance of not decending, first wrong order
         if (firstElement == null && root.val < lastElement.val) {
             firstElement = lastElement;
         }
+        // the rest that are not assending, alwasy updating th elast one
         if (firstElement != null && root.val < lastElement.val) {
             secondElement = root;
         }
@@ -19,7 +28,7 @@
     }
 
     public void recoverTree(TreeNode root) {
-        // traverse and get two elements
+        // in order traverse and get two elements
         traverse(root);
         // swap
         int temp = firstElement.val;
