b920522ec01b15e53c7f655de945aa57e38d2bce
siliang625 <siliang625@users.noreply.github.com>
2018-07-14 10:23:45 -0700

    20180714

--- p_string.java
+++ p_string.java
@@ -1,7 +1,50 @@
 //duplicate
 //keep 1
+public String duplicate1(String str){
+  if(str == null || str.length() < 2)
+    return str;
+  char[] arr = str.toCharArray();
+  int slow = 0;
+  int fast = 1;
+  for(fast = 1; fast < arr.length; i++){
+    if(arr[slow] != arr[fast])
+      arr[++slow] = arr[fast];
+  }
+  return new String(arr, 0, slow+1);
+}
 //keep 2
+public String duplicate2(String str){
+  if(str == null || str.length() < 2)
+    return str;
+  char[] arr = str.toCharArray();
+  int slow = 0;
+  int fast = 1;
+  int coutner = 1;
+  for(fast = 1; fast < arr.length; i++){
+    if(arr[slow] != arr[fast])
+      arr[++slow] = arr[fast];
+    else{
+      if(counter < 2){
+        arr[++slow] = arr[fast];
+        counter++;
+      }
+    }
+  }
+  return new String(arr, 0, slow+1);
+}
 //keep 0
+public String duplicate1(String str){
+  if(str == null || str.length() < 2)
+    return str;
+  char[] arr = str.toCharArray();
+  int slow = 0;
+  int fast = 1;
+  for(fast = 1; fast < arr.length; i++){
+    if(arr[slow] != arr[fast])
+      arr[++slow] = arr[fast];
+  }
+  return new String(arr, 0, slow+1);
+}
 //zuma
 
 //remove
@@ -20,4 +63,4 @@
 //isomorphic
 
 
-//reorder: ABCD1234
+//shuffle: ABCD1234
