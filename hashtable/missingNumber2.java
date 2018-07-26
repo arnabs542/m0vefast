a29694d60ad488266fa18145c2c670158063674e
siliang625 <siliang625@users.noreply.github.com>
2018-07-25 22:02:13 -0700

    20180725

--- hashtable/missingNumber2.java
+++ hashtable/missingNumber2.java
@@ -28,3 +28,27 @@ left=0
 right=2
 mid=1
 left=2
+  //if numebr starting from 0, then target is mid not mid+1
+public int findMissing(int[] arr){
+  int left = 0;
+  int right = arr.length-1;
+  //edge cases:
+  if(arr == null){
+    return -1;
+  }
+  //missing first one and empty arr: covered
+  //missing last one
+  if(arr[right] == right){
+    return right+1;
+  }
+  //normal case:
+  while(left<right){
+    int mid = left + (right - left)/2;
+    if(arr[mid] <= mid){
+      left = mid+1;
+    }else{
+      right = mid;  
+    }
+  }
+  return left;
+}
